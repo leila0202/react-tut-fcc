b27ff5d2364258e189f705786c770a1d97948673
Leila Zrim <leilazrim@gmail.com>
2023-03-20 09:42:12 +0100

    added example of not useable js in jsx

--- 01-react-fundamentals/src/index.js
+++ 01-react-fundamentals/src/index.js
@@ -26,6 +26,8 @@ const Book = () => {
       />
       <h2>{title}</h2>
       <h4>{author}</h4>
+      {/* <p>{let x = 6}</p> doesn't work it must be an expression (return value)*/}{' '}
+      /
     </article>
   );
 };
