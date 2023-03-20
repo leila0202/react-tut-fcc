fe7d21c04a7d867691b5d676946d981ab4465029
Leila Zrim <leilazrim@gmail.com>
2023-03-20 11:21:56 +0100

    use spread (...) operator to pass the props

--- 01-react-fundamentals/src/index.js
+++ 01-react-fundamentals/src/index.js
@@ -9,12 +9,14 @@ const books = [
     title: 'Atomic Habits',
     imgUrl:
       'https://images-na.ssl-images-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg',
+    id: 1,
   },
   {
     author: 'Dav Pilkey',
     title: 'Dog Man: Twenty Thousand Fleas Under the Sea',
     imgUrl:
       'https://images-na.ssl-images-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81fyoFoaxlL._AC_UL600_SR600,400_.jpg',
+    id: 2,
   },
 ];
 
@@ -22,10 +24,7 @@ const BookList = () => {
   return (
     <section className="booklist">
       {books.map((book) => {
-        const { title, author, imgUrl } = book;
-        return (
-          <Book key={title} title={title} author={author} imgUrl={imgUrl} />
-        );
+        return <Book key={book.id} {...book} />;
       })}
     </section>
   );
