9dce81d72e610b9b0265cb79afcec38ab1107ef5
Leila Zrim <leilazrim@gmail.com>
2023-03-20 09:52:21 +0100

    added props for title, author and imgurl (still kinda static)

--- 01-react-fundamentals/src/index.js
+++ 01-react-fundamentals/src/index.js
@@ -3,31 +3,29 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
+const author = 'James Clear';
+const title = 'Atomic Habits'; //can be declared insinde or outside of the componentn but not in the return statement
+const imgUrl =
+  'https://images-na.ssl-images-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg';
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book author={author} title={title} imgUrl={imgUrl} />
+      <Book author={author} title={title} imgUrl={imgUrl} />
+      <Book author={author} title={title} imgUrl={imgUrl} />
     </section>
   );
 };
 
-const author = 'James Clear';
-
-const Book = () => {
-  const title = 'Atomic Habits'; //can be declared insinde or outside of the componentn but not in the return statement
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg"
-        alt={title + ' Cover'}
-      />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-      {/* <p>{let x = 6}</p> doesn't work it must be an expression (return value)*/}{' '}
-      /
+      <img src={props.imgUrl} alt={props.title + ' Cover'} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
+      {/* <p>{let x = 6}</p> doesn't work it must be an expression (return value)*/}
     </article>
   );
 };
