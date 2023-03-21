8d4112415f4e20a5904b74efa60ba0a16caf0d52
Leila Zrim <leilazrim@gmail.com>
2023-03-21 14:38:00 +0100

    use State basic example 2

--- 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,25 @@
+import { useState } from 'react';
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  // console.log(useState(1));
+  // const value = useState('hello')[0];
+  // const func = useState('hello')[1];
+  // console.log(value);
+  // console.log(func);
+
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <>
+      <h2>{count}</h2>
+      <button className="btn" onClick={handleClick}>
+        increase
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;
