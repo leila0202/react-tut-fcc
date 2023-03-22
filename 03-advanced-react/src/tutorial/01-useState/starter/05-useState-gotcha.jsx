c8fd4d86e2b976fad4b9cca291bc11cf9ae547b3
Leila Zrim <leilazrim@gmail.com>
2023-03-22 13:17:35 +0100

    refactor example 5 state to use the function für setState

--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -2,18 +2,18 @@ import { useState } from 'react';
 
 const UseStateGotcha = () => {
   const [number, setNumber] = useState(0);
+  const handleClick = () => {
+    setNumber((prevState) => {
+      const newState = prevState + 1;
+      return newState;
+    });
+    console.log(newState);
+  };
   return (
     <>
       <h2>useState "gotcha"</h2>
       <h3>{number}</h3>
-      <button
-        type="button"
-        className="btn"
-        onClick={() => {
-          setNumber(number + 1);
-          console.log(number);
-        }}
-      >
+      <button type="button" className="btn" onClick={handleClick}>
         increase
       </button>
     </>
