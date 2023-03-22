06c5e32b74b0ea1ee71723341528bd2d038d8d05
Leila Zrim <leilazrim@gmail.com>
2023-03-22 13:35:19 +0100

    useEffect 02 example solve problem from 01 with useEffect Hook

--- 03-advanced-react/src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
@@ -7,11 +7,13 @@ const UseEffectBasics = () => {
   };
 
   sayHello();
-
+  useEffect(() => {
+    console.log('hello from use effect');
+  }, []);
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>
