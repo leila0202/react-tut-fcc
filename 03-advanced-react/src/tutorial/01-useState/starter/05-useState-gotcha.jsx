ef2a01f2a08494ef1c49f25b3ffdbac3f5b3e0cb
Leila Zrim <leilazrim@gmail.com>
2023-03-22 12:57:05 +0100

    state example 5 (own - seems there is a catch - number doesn't change immediately in console log)

--- 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [number, setNumber] = useState(0);
+  return (
+    <>
+      <h2>useState "gotcha"</h2>
+      <h3>{number}</h3>
+      <button
+        type="button"
+        className="btn"
+        onClick={() => {
+          setNumber(number + 1);
+          console.log(number);
+        }}
+      >
+        increase
+      </button>
+    </>
+  );
 };
 
 export default UseStateGotcha;
