17439e3764afdc7f8d5f4e97fea2d6e7715a6263
Leila Zrim <leilazrim@gmail.com>
2023-03-22 10:34:52 +0100

    state example 4 withut object on my own

--- 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,25 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState('Kathi');
+  const [age, setAge] = useState(24);
+  const [hobby, setHobby] = useState('hiking');
+  const handleClick = () => {
+    setName('Leila');
+    setAge(29);
+    setHobby('programming');
+  };
+  return (
+    <div>
+      <h2>useState object example</h2>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{`Enjoys ${hobby}`}</h3>
+      <button type="button" onClick={handleClick}>
+        Show Leila
+      </button>
+    </div>
+  );
 };
 
 export default UseStateObject;
