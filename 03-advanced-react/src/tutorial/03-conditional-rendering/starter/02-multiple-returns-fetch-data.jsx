7fe219d534efd832ece9bc9c68397eebd9f84ad5
Leila Zrim <leilazrim@gmail.com>
2023-03-23 08:55:05 +0100

    02 multiple returns challange 1 finished on my own - practise fetching data

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,20 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const user = await response.json();
+        setUser(user);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+  console.log(user);
   return <h2>Fetch Data </h2>;
 };
 export default MultipleReturnsFetchData;
