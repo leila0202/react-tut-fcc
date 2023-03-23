9330599138f8bea3d44684e29a686aa8acad88b7
Leila Zrim <leilazrim@gmail.com>
2023-03-23 11:57:29 +0100

    07 conditional rendering user challange own solution, not checked with video

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,35 @@
+import { useState } from 'react';
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+  const login = () => {
+    setUser({
+      name: 'Leila',
+    });
+  };
+  const logout = () => {
+    setUser(null);
+  };
+  return (
+    <div>
+      <h2>user challenge</h2>
+      {user ? (
+        <div>
+          <h4> Hello there, {user.name}</h4>{' '}
+          <button type="button" onClick={logout}>
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h4>Please Login</h4>
+          <button type="button" onClick={login}>
+            Login
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default UserChallenge;
