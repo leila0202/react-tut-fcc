2782e8f1c86305c98ea0b6170d2aa9fa041fcf9e
Leila Zrim <leilazrim@gmail.com>
2023-03-23 09:15:24 +0100

    fixed mistake of placement of destructuring the user object

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -3,19 +3,39 @@ const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
   const [user, setUser] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(url);
         const user = await response.json();
+
         setUser(user);
       } catch (error) {
         console.log(error);
+        setIsError(true);
       }
+
+      setIsLoading(false);
     };
     fetchData();
   }, []);
-  console.log(user);
-  return <h2>Fetch Data </h2>;
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2 style={{ color: 'red' }}>Error</h2>;
+  }
+  const { avatar_url, name, company, bio } = user;
+  return (
+    <div className="container">
+      <h2>Fetch Data </h2>
+      <img src={avatar_url} alt={name} />
+      <h3>{name}</h3>
+      <h4>{company}</h4>
+      <h4>{bio}</h4>
+    </div>
+  );
 };
 export default MultipleReturnsFetchData;
