e5b4e8e6d7458ee7a9da4ebd5d9bebca648fd103
Leila Zrim <leilazrim@gmail.com>
2023-03-23 15:25:49 +0100

    project structure example

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,9 +1,13 @@
-import Starter from './tutorial/02-useEffect/starter/05-cleanup-function';
+// import Starter from './tutorial/02-useEffect/starter/05-cleanup-function';
+
+import { Home, About } from './tutorial/04-project-structure/starter/Pages';
 
 function App() {
   return (
     <div className="container">
-      <Starter />
+      {/* <Starter /> */}
+      <Home />
+      <About />
     </div>
   );
 }
