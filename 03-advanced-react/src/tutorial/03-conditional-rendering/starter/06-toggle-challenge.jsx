2bd6dedc6455895a3b41a0981e10788e6a2f048c
Leila Zrim <leilazrim@gmail.com>
2023-03-23 11:47:21 +0100

    06 conditional rendering toggle challange added my own version (is correct but not the same as readme)

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,16 @@
+import { useState } from 'react';
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [showAlert, setShowAlert] = useState(true);
+  return (
+    <div>
+      <h2>toggle challenge</h2>
+      {showAlert && <h4>This is an Alert!!!!!!</h4>}
+      <button type="button" onClick={() => setShowAlert(!showAlert)}>
+        {showAlert ? 'Hide' : 'Show'}
+      </button>
+    </div>
+  );
 };
 
 export default ToggleChallenge;
