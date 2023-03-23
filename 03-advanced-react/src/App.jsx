1e725977432b3fdfc52e28f21ae9848421e312e1
Leila Zrim <leilazrim@gmail.com>
2023-03-23 15:31:15 +0100

    grouped export

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,13 +1,12 @@
 // import Starter from './tutorial/02-useEffect/starter/05-cleanup-function';
 
-import { Home, About } from './tutorial/04-project-structure/starter/Pages';
+import Example from './tutorial/04-project-structure/starter/Example';
 
 function App() {
   return (
     <div className="container">
       {/* <Starter /> */}
-      <Home />
-      <About />
+      <Example />
     </div>
   );
 }
