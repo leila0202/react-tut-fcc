638ab555856287413c20c45b281ae4e9b56cca20
Leila Zrim <leilazrim@gmail.com>
2023-03-24 14:08:34 +0100

    forms multiple inputs, with dynamic properties

--- 03-advanced-react/src/tutorial/06-forms/starter/03-multiple-inputs.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/03-multiple-inputs.jsx
@@ -1,31 +1,77 @@
+import { useState } from 'react';
+
 const MultipleInputs = () => {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const property = e.target.name;
+    const value = e.target.value;
+    setUser({ ...user, [property]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!user.name || !user.email || !user.email) return;
+    console.log(user);
+    setUser({
+      name: '',
+      email: '',
+      password: '',
+    });
+  };
+
   return (
     <div>
-      <form className='form'>
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Multiple Inputs</h4>
         {/* name */}
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={user.name}
+            name="name"
+            onChange={handleChange} //event is automatically available no need for arrow function
+          />
         </div>
         {/* email */}
-        <div className='form-row'>
-          <label htmlFor='email' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
             Email
           </label>
-          <input type='email' className='form-input' id='email' />
+          <input
+            type="email"
+            className="form-input"
+            id="email"
+            value={user.email}
+            name="email"
+            onChange={handleChange}
+          />
         </div>
         {/* email */}
-        <div className='form-row'>
-          <label htmlFor='password' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="password" className="form-label">
             Password
           </label>
-          <input type='password' className='form-input' id='password' />
+          <input
+            type="password"
+            className="form-input"
+            id="password"
+            value={user.password}
+            name="password"
+            onChange={handleChange}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
