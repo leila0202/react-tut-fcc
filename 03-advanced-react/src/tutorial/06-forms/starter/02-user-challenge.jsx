dc0a99edb2270104a0a55e9667c5b94bc6dc0fc1
Leila Zrim <leilazrim@gmail.com>
2023-03-24 11:44:57 +0100

    tried solving the form user challange on my own (unsure about the set state function)

--- 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ 03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,20 +1,59 @@
+import { useState } from 'react';
+import { data } from '../../../data';
+
 const UserChallenge = () => {
+  const [name, setName] = useState('');
+  const [users, setUsers] = useState(data);
+  const id = users[users.length - 1].id;
+  console.log(id);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newUser = {
+      id: id + 1,
+      name: name,
+    };
+    setUsers((prevState) => {
+      return prevState.concat(newUser);
+    });
+  };
+
+  const removeUser = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
   return (
     <div>
-      <form className='form'>
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
       {/* render users below */}
+      {users.map((user) => (
+        <div key={user.id}>
+          <h4>{user.name}</h4>
+          <button
+            type="button"
+            className="btn"
+            onClick={() => removeUser(user.id)}
+          >
+            Remove
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
