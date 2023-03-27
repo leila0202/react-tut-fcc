150d6d104cd34dbe7b874ff8dc8c9ce146cdc899
Leila Zrim <leilazrim@gmail.com>
2023-03-27 08:33:38 +0200

    06 use ref second example (focus, setting ref ourself)

--- 03-advanced-react/src/tutorial/07-useRef/starter/01-useRef-basics.jsx
+++ 03-advanced-react/src/tutorial/07-useRef/starter/01-useRef-basics.jsx
@@ -3,10 +3,19 @@ import { useEffect, useRef, useState } from 'react';
 const UseRefBasics = () => {
   const [value, setValue] = useState(0);
   const refContainer = useRef(null);
-  console.log(refContainer);
-  // useEffect(() => {
-  //   console.log(refContainer);
-  // });
+  const isMounted = useRef(false);
+
+  useEffect(() => {
+    refContainer.current.focus();
+  });
+
+  useEffect(() => {
+    if (!isMounted.current) {
+      isMounted.current = true;
+      return;
+    }
+    console.log('re-render');
+  }, [value]);
   const handleSubmit = (e) => {
     e.preventDefault();
     const name = refContainer.current.value;
