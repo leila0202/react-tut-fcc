2c5ca22d941c52f17856de802601fd49981c1788
Leila Zrim <leilazrim@gmail.com>
2023-03-28 09:44:05 +0200

    10 useReducer added boilerplate for reducer

--- 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ 03-advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import { useReducer } from 'react';
 import { data } from '../../../data';
+
+const defaultState = {
+  people: data,
+};
+
+const reducer = () => {};
+
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    // let newPeople = people.filter((person) => person.id !== id);
+    // setPeople(newPeople);
   };
 
   const clearList = () => {
-    setPeople([]);
+    // setPeople([]);
   };
 
   const resetList = () => {
-    setPeople(data);
+    // setPeople(data);
   };
+  console.log(state);
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className="item">
@@ -26,7 +34,7 @@ const ReducerBasics = () => {
           </div>
         );
       })}
-      {people.length > 1 ? (
+      {state.people.length > 1 ? (
         <button
           className="btn"
           style={{ marginTop: '2rem' }}
