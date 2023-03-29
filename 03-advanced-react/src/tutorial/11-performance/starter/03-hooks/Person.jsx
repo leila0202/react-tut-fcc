5d0471a61fc65aae36c921b90c9112879f48b65e
Leila Zrim <leilazrim@gmail.com>
2023-03-29 10:40:09 +0200

    constructed a removePerson function and passed it via state down. now everything rerenders again, we need to solve that

--- 03-advanced-react/src/tutorial/11-performance/starter/03-hooks/Person.jsx
+++ 03-advanced-react/src/tutorial/11-performance/starter/03-hooks/Person.jsx
@@ -1,7 +1,10 @@
-const Person = ({ name }) => {
+const Person = ({ name, id, removePerson }) => {
   return (
     <div>
       <h4>{name}</h4>
+      <button type="button" onClick={() => removePerson(id)}>
+        Remove
+      </button>
     </div>
   );
 };
--- 03-advanced-react/src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ 03-advanced-react/src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -4,6 +4,10 @@ import List from './List';
 const LowerState = () => {
   const [people, setPeople] = useState(data);
   const [count, setCount] = useState(0);
+  const removePerson = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
 
   return (
     <section>
@@ -14,7 +18,7 @@ const LowerState = () => {
       >
         count {count}
       </button>
-      <List people={people} />
+      <List people={people} removePerson={removePerson} />
     </section>
   );
 };
